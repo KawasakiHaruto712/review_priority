2510b0f221d5e1d214660509d1acd7796ecc225c
KawasakiHaruto712 <wakayama.kawasaki@gmail.com>
2025-07-30 17:33:39 +0900

    [add]collectorsとpreprocessingのテストを追加
    testsディレクトリにデータ収集時とデータの前処理時に用いるコードのテストを追加

---
+++ tests/preprocessing/__init__.py
@@ -0,0 +1,12 @@
+"""
+tests.preprocessing
+
+preprocessing モジュールのテストスイート
+
+テスト対象:
+- review_comment_processor: レビューコメントの前処理とキーワード抽出
+
+実行方法:
+    pytest tests/preprocessing/
+    python -m unittest discover tests/preprocessing/
+"""
