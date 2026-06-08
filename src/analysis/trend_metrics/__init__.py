7dc3986d4374e2475e00b698ecbfb1820db42006
KawasakiHaruto712 <wakayama.kawasaki@gmail.com>
2026-06-09 01:26:35 +0900

    [fix]不要なファイルを削除

--- src/analysis/trend_metrics/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Trend Metrics Analysis パッケージ
-前期/後期でメトリクスの値がどのように変化するかを分析
-"""
-
-from .main import TrendMetricsAnalyzer
-
-__all__ = ['TrendMetricsAnalyzer']
--- src/analysis/trend_models/classification_models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Trend Models Analysis Classification Models"""
-
-from src.analysis.trend_models.classification_models.base_model import BaseModel
-from src.analysis.trend_models.classification_models.trend_predictor import TrendPredictor
-
-__all__ = ['BaseModel', 'TrendPredictor']
